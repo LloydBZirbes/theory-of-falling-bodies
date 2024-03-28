658fff58ead7b2a992623b81215ff7e6b54fe179
Jen Brannstrom <jen@rtb.cat>
2024-03-28 16:37:20 +0200

    all files

---
+++ original/assemblies/pic.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+            <title>Display Picture</title>
+</head>
+<body onload=window.focus()>
+      <center>
+      		<img src="<?echo @$_GET["pic_url"]?>">
+      		<br><br>
+      		<form>
+      			<input type="Button" OnClick="self.close();" value="Close Window">
+      		</form>
+      </center>
+</body>
+</head>
+</html>
